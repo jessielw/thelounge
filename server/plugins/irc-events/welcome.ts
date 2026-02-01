a123461a843b1731196f0c1fc625df78e9089269
jessielw <jessielw4049@gmail.com>
2026-02-01 17:36:34 -0500

    Fix state machine to preserve preferred nick without fallbacks
    - error.ts: Store user's preferred nick (network.nick) in keepNick, not session nick
    - error.ts: Remove unnecessary client nick emit after fallback
    - welcome.ts: Don't overwrite preferred nick when registered with fallback
    - Allows existing quit handler to naturally reclaim preferred nick when it becomes available

--- server/plugins/irc-events/welcome.ts
+++ server/plugins/irc-events/welcome.ts
@@ -6,7 +6,19 @@ export default <IrcEventHandler>function (irc, network) {
 	const client = this;
 
 	irc.on("registered", function (data) {
-		network.setNick(data.nick);
+		// Only update the user's preferred nick (network.nick) if we registered with it
+		// If we registered with a fallback nick (e.g., nick123), don't overwrite the preference
+		// This allows the existing quit handler to reclaim the preferred nick when available
+		if (data.nick === network.nick) {
+			// We got our preferred nick, clear keepNick if it was set
+			if (network.keepNick === data.nick) {
+				network.keepNick = null;
+			}
+		} else if (data.nick !== network.nick) {
+			// We registered with a fallback, don't call setNick which would overwrite the preference
+			// Just update the IRC session nick
+			irc.user.nick = data.nick;
+		}
 
 		const lobby = network.getLobby();
 		const msg = new Msg({
